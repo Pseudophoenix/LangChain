c9295d8922e3a0dbc656ca0187d903a3c17326cb
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-06 18:19:51 +0530

    Added

--- langchain_trial/6. Output Parser/pydantic_demo.py
+++ langchain_trial/6. Output Parser/pydantic_demo.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 class Student(BaseModel):
-    name:str='Alok'
-new_student={}
+    name:str='Alok' # default value set as Alok
+    age:Optional[int]=None
+    
+new_student={"age":45}
 stude=Student(**new_student)
-print(stude)
+print(stude.name)
+print(stude.age)
