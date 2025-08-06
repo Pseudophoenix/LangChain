857e845ea0b57492faab9dcf991d23238521fd78
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-07 02:42:03 +0530

    RunnableBranch added

--- langchain_trial/8. Runnables/6. runnable_lambda.py
+++ langchain_trial/8. Runnables/6. runnable_lambda.py
@@ -1,2 +1,34 @@
 # RunnableLambda-allows use to apply custom Python functions within an AI pipeline
 # acts as a middleware between different AI components, enabling preprocessing, transformation, API calls, filtering, and post-processing in a LangChain Workflow.
+from langchain.schema.runnable import RunnableLambda, RunnableSequence, RunnableParallel, RunnablePassthrough
+from langchain_google_vertexai import ChatVertexAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+def word_counter(text):
+    return len(text.split())
+# runnable_word_counter=RunnableLambda(word_counter)
+# print(runnable_word_counter.invoke("Hi there how are you ?"))
+
+prompt=PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=['topic']
+)
+model=ChatVertexAI(model_name="gemini-2.5-flash")
+parser=StrOutputParser()
+joke_gen_chain=RunnableSequence(prompt|model|parser)
+
+parallel_chain=RunnableParallel({
+    'joke':RunnablePassthrough(),
+    # 'word_count':RunnableLambda(word_counter)
+    "word_count":RunnableLambda(lambda x:len(x.split()))
+})
+final_chain=RunnableSequence(joke_gen_chain|parallel_chain)
+result=final_chain.invoke({'topic':'AI'})
+final_res="""
+{}\n word count - {}""".format(result['joke'],result['word_count'])
+print(final_res)
+# print(res.joke)
+# print(res.word_count)
---
+++ langchain_trial/8. Runnables/7. runnable_branch.py
@@ -0,0 +1,28 @@
+# a control flow component in LangChain that allows us to conditionally route input data to different chains or runnables based on custom logic
+# It functions like an if/elif/else block for chains - where we define a set of condition functions, each associated with a runnable(e.g LLM call, prompt chain, or tool). The first matching condition is executed. If no conition matches, a default runnable is used (if provided).
+from langchain_google_vertexai import ChatVertexAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
+load_dotenv()
+
+prompt=PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=['topic']
+)
+prompt2=PromptTemplate(
+    template="Summarize the following text\n{text}",
+    input_variables=['text']
+)
+model=ChatVertexAI(model_name="gemini-2.5-flash")
+parser=StrOutputParser()
+report_gen_chain=RunnableSequence(prompt,model,parser)
+branch_chain=RunnableBranch(
+    # (condition,runnable),
+    (lambda x:len(x.split())>300,RunnableSequence(prompt2,model,parser)),
+    RunnablePassthrough()
+)
+final_chain=RunnableSequence(report_gen_chain,branch_chain)
+res=final_chain.invoke({'topic':'Russia Ukraine War'})
+print(res)
