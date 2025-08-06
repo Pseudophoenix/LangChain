133c948d39e829b41646f4c753ee48babef6f334
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-06 18:29:24 +0530

    Added

--- langchain_trial/6. Output Parser/pydantic_demo.py
+++ langchain_trial/6. Output Parser/pydantic_demo.py
@@ -1,10 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 class Student(BaseModel):
     name:str='Alok' # default value set as Alok
     age:Optional[int]=None # Optional value
-    
-new_student={"age":"45"} # Coercing - string converted implicitly into int by pydantic
+    email:Optional[EmailStr]='abc@gmail.com'
+    cgpa:float=Field(gt=0,lt=10,default=5,description="Decimal value representing the CGPA of the student")
+new_student={
+    "age":"45","email":"abc@gmail.com"
+            #  ,"cgpa":9
+} # Coercing - string converted implicitly into int by pydantic
+# EmailStr is a builtin class provided for validation of Email address
+# FieldFunction -> default values, constraints, RegEx
 stude=Student(**new_student)
-print(stude.name)
-print(type(stude.age))
+stude_json=stude.model_dump_json()
+stude_dict=dict(stude)
+print(stude_dict)
+print(stude_json)
+print(stude)
