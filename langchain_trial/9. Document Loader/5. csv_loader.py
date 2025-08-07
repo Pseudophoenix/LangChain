32dad95783719725fee2b4e0f8064cd8fdd89afe
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-07 18:48:53 +0530

    Text Splitters added

--- langchain_trial/9. Document Loader/5. csv_loader.py
+++ langchain_trial/9. Document Loader/5. csv_loader.py
@@ -2,3 +2,4 @@ from langchain_community.document_loaders import CSVLoader
 loader=CSVLoader(file_path="")
 data=loader.load()
 print(data[0].page_content)
+# We can even create our custom DATALOADERS
