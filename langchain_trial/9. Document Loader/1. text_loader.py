555a53c33e15a32585a9c391e67ea8a2101e1130
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-07 16:50:19 +0530

    Added

--- langchain_trial/9. Document Loader/1. text_loader.py
+++ langchain_trial/9. Document Loader/1. text_loader.py
@@ -1,1 +1,4 @@
 from langchain_community.document_loaders import TextLoader
+loader=TextLoader('9. Document Loader\document_text.txt',encoding='utf-8')
+doc=loader.load()
+print(type(doc[0])) # a list of docs - <class 'langchain_core.documents.base.Document'>
