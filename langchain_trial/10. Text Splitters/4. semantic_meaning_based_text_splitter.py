e47ec98fbbe492167fa61634ed5e0d11ef0410e2
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-07 22:23:38 +0530

    LFS handled

--- langchain_trial/10. Text Splitters/4. semantic_meaning_based_text_splitter.py
+++ langchain_trial/10. Text Splitters/4. semantic_meaning_based_text_splitter.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 text_splitter=SemanticChunker(
     VertexAIEmbeddings(model_name="gemini-embedding-001"),breakpoint_threshold_type="standard_deviation",
-    breakpoint_threshold_amount=1.2
+    breakpoint_threshold_amount=1.3
 )
 sample="""
 Farmers were working hard in the fields, preparing the soil and planting seeds for the next season. The sun was bright, and the air smelled of earth and fresh grass. The Indian Premier League (IPL) is the biggest cricket league in the world. People all over the world watch the matches and cheer for their favourite teams.
