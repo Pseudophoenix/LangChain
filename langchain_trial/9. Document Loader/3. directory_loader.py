90e9f7678972d45cf57359b772c4f2887f5533de
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-07 18:13:21 +0530

    Added

--- langchain_trial/9. Document Loader/3. directory_loader.py
+++ langchain_trial/9. Document Loader/3. directory_loader.py
@@ -1,3 +1,13 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_google_vertexai import ChatVertexAI
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 
+loader=DirectoryLoader(
+    path="books",
+    glob='*.pdf',
+    loader_cls=PyPDFLoader
+)
+docs=loader.load()
+# print(len(docs))
+print(docs[1320].page_content)
+print(docs[1320].metadata)
