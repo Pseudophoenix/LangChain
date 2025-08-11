96497826efc9f94fc1da772a0ee587f7d376b954
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-12 00:26:36 +0530

    Streamlit Bot added

--- langgraph-trial/ChatBot/streamlit_frontend.py
+++ langgraph-trial/ChatBot/streamlit_frontend.py
@@ -20,7 +20,7 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response=chatbot.invoke({'messsages':[HumanMessage(content=user_input)]},config=CONFIG)
+    response=chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config=CONFIG)
     ai_message=response['messages'][-1].content
     st.session_state['message_history'].append({'role':'assistant',"content":ai_message})
     with st.chat_message('assistant'):
