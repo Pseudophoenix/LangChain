dc8e6d0d550554ac2a7225f9a7d22d838cd06bd9
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-12 10:54:15 +0530

    Database backend added

--- langgraph-trial/ChatBot/langgraph_database_backend.py
+++ langgraph-trial/ChatBot/langgraph_database_backend.py
@@ -2,14 +2,14 @@ from langchain_google_vertexai import ChatVertexAI
 from dotenv import load_dotenv
 from typing import TypedDict, Annotated
 from langgraph.graph import StateGraph, START, END
-from langchain_core.messages  import BaseMessage
+from langchain_core.messages  import BaseMessage, HumanMessage
 from langgraph.graph.message import add_messages
 # from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3 
 load_dotenv()
 llm=ChatVertexAI(model_name="gemini-2.5-pro")
-sqlite3.connect(database="chatbot.db",check_same_thread=False)
+conn=sqlite3.connect(database="chatbot.db",check_same_thread=False)
 
 class ChatState(TypedDict):
     messages:Annotated[list[BaseMessage],add_messages]
@@ -21,7 +21,7 @@ def chat_node(state:ChatState):
     return {"messages":[response]}
 
 # checkpointer=InMemorySaver()
-checkpointer=SqliteSaver()
+checkpointer=SqliteSaver(conn=conn)
 graph=StateGraph(ChatState)
 
 graph.add_node("chat_node",chat_node)
@@ -30,3 +30,9 @@ graph.add_edge(START,"chat_node")
 graph.add_edge("chat_node",END)
 
 chatbot=graph.compile(checkpointer=checkpointer)
+
+# response=chatbot.invoke({
+#     'messages':[HumanMessage(content="What is my capital of USA")]
+# },config={"configurable":{"thread_id":"2"}})
+
+# print(response)
