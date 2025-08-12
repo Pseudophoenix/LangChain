4620cb6661c57b41ec1dc1139b618fd2a556bf6b
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-12 09:35:09 +0530

    Streamlit Bot Resume Chat updated

--- langgraph-trial/ChatBot/streamlit_frontend_sidebar_threading.py
+++ langgraph-trial/ChatBot/streamlit_frontend_sidebar_threading.py
@@ -5,26 +5,28 @@ import uuid
 
 # **************************************** utility functions *************************
 
+# generate the thread_id
 def generate_thread_id():
     thread_id = uuid.uuid4()
     return thread_id
-
+# reset means generate a new thread_id and make it as the current thread_id also add it to chat_threads sesion_state, make its message history as blank
 def reset_chat():
     thread_id = generate_thread_id()
     st.session_state['thread_id'] = thread_id
     add_thread(st.session_state['thread_id'])
     st.session_state['message_history'] = []
-
+# add the given thread_id to chat_threads session_state
 def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
         st.session_state['chat_threads'].append(thread_id)
 
+# from the states of chat bot fetch the messages of that particular thread id and return those
 def load_conversation(thread_id):
     if "messages" in chatbot.get_state(config={"configurable":{'thread_id':thread_id}}).values:
         return chatbot.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
     return []
 
-# **************************************** Session Setup ******************************
+# *********** Session Setup ***************
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
@@ -33,37 +35,40 @@ if 'thread_id' not in st.session_state:
 
 if 'chat_threads' not in st.session_state:
     st.session_state['chat_threads'] = []
-
+# add the current thread_id to the chat_threads session_state
 add_thread(st.session_state['thread_id'])
 
 
-# **************************************** Sidebar UI *********************************
+# ********************* Sidebar UI *******************
 
 st.sidebar.title('LangGraph Chatbot')
-
+# on button click reset the chat that is create a fresh thread_id and make its initial configuration
 if st.sidebar.button('New Chat'):
     reset_chat()
 
 st.sidebar.header('My Conversations')
-
+# displau all the chats in the sidebar use the chat_threads and fetch all of the thread_ids 
 for thread_id in st.session_state['chat_threads'][::-1]:
+    # create a separate clickable button for each thread_id
     if st.sidebar.button(str(thread_id)):
+        # make that particular thread_id as the current thread_id and load on screen
         st.session_state['thread_id'] = thread_id
         messages = load_conversation(thread_id)
-
+        # messaages now has those messages
         temp_messages = []
-
+        # from the messages assign role to the chat message depending on whether it is an instanceof HumanMessage or Not
         for msg in messages:
             if isinstance(msg, HumanMessage):
                 role='user'
             else:
                 role='assistant'
+            # temp messages will store the role and content 
             temp_messages.append({'role': role, 'content': msg.content})
-
+        # change the entire message history session_state replacing it with temp_messages
         st.session_state['message_history'] = temp_messages
 
 
-# **************************************** Main UI ************************************
+# ********************** Main UI ****************
 
 # loading the conversation history
 for message in st.session_state['message_history']:
